287cf4de1da3ad4044c852f320ff9f15f00cbf7d
shane-david <shanepdavid06@gmail.com>
2026-02-25 22:46:44 -0700

    build

--- Assets/Scripts/ExitGame.cs
+++ Assets/Scripts/ExitGame.cs
@@ -1,14 +1,9 @@
 using UnityEngine;
-
 public class ExitGame : MonoBehaviour
 {
     public void exitApplication() {
 
-        if (Application.isEditor) //If App Is Running In Editor, Stop Playing The Scene
-        {
-            UnityEditor.EditorApplication.isPlaying = false;
-        }
-        else Application.Quit(); //If App Is Not Running In Editor, Quit The Application
+        Application.Quit(); //If App Is Not Running In Editor, Quit The Application
     }
 
 }
